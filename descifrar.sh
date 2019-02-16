65618bf3d92af92c0939d6e623a80b6a90747945
hackingyseguridad <antonio_taboada@hotmail.es>
2019-02-17 00:30:18 +0100

    Update descifrar.sh

--- descifrar.sh
+++ descifrar.sh
@@ -1,2 +1,20 @@
-
-openssl aes-256-cbc -d -a -in secreto.txt.enc -out secreto.txt.new
+#!/bin/bash
+cat << "INFO"
+   _____ _  __                
+  / ____(_)/ _|               
+ | |     _| |_ _ __ __ _ _ __ 
+ | |    | |  _| '__/ _` | '__|
+ | |____| | | | | | (_| | |   
+  \_____|_|_| |_|  \__,_|_|   V.1.0 
+   http://www.hackingyseguridad.com
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Cifrar fichero"
+        echo "Uso: $0 <fichero>"
+        exit 0
+fi
+echo
+echo "Cifrado del fichero : " $1
+echo
+openssl aes-256-cbc -d -a -in $1 -out $1.new
