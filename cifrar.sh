30014d13e52153825033c9127883aef58544980b
hackingyseguridad <antonio_taboada@hotmail.es>
2019-02-17 00:06:56 +0100

    Update cifrar.sh

--- cifrar.sh
+++ cifrar.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 cat << "INFO"
-   __                          _     _   _             
-  / _|                        | |   | | | |            
- | |_ _   _ ___________ _ __  | |__ | |_| |_ _ __  ___ 
- |  _| | | |_  /_  / _ \ '__| | '_ \| __| __| '_ \/ __|
- | | | |_| |/ / / /  __/ |    | | | | |_| |_| |_) \__ \
- |_|  \__,_/___/___\___|_|    |_| |_|\__|\__| .__/|___/ v 2.0
-                                            | |        
-         http://www.hackingyseguridad.com   |_|        
+   _____ _  __                
+  / ____(_)/ _|               
+ | |     _| |_ _ __ __ _ _ __ 
+ | |    | |  _| '__/ _` | '__|
+ | |____| | | | | | (_| | |   
+  \_____|_|_| |_|  \__,_|_|   V.1.0 
+   http://www.hackingyseguridad.com
+
 INFO
 if [ -z "$1" ]; then
         echo
-        echo "Genera CA root y certificados para las peticiones https "
-        echo "Descubre ficheros en  url de sitio web por 200 OK.. "
-        echo "Uso: $0 <https://dominio.com>"
-        echo "Tiempo estimado 1 hora ..."
+        echo "Cifrar fichero "
+        echo "Uso: $0 <fichero>"
         exit 0
 fi
 echo
-echo "Fuzzer de: " $1
+echo "Cifrado del fichero : " $1
 echo
 
 openssl aes-256-cbc -a -salt -in $1 -out secreto.txt.enc
